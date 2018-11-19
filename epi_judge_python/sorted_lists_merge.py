b81a7a926bf7378e2499750785c853ec561a6d5d
Aurea <fu.sakura@gmail.com>
2018-11-19 09:01:19 -0800

    working copy sort merged linked list

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def merge_two_sorted_lists(L1, L2):
-    # TODO - you fill in here.
-    return None
+
+    L1_pt, L2_pt = L1, L2
+
+
+    if L1_pt.data <= L2_pt.data:
+        curr_least = L1
+        head = L1
+        L1_pt = L1_pt.next
+    else:
+        curr_least = L2
+        head = L2
+
+
+    while L1_pt != L2_pt:
+
+        if L
+
+
+    return head
 
 
 if __name__ == '__main__':
