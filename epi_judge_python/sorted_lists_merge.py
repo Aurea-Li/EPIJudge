0b4ada2ef6e006cc506a30651ba7856b9d7abd4a
Aurea <fu.sakura@gmail.com>
2018-11-27 15:55:43 -0800

    add sorted lists merge algorithm for linked lists

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -1,26 +1,38 @@
 from test_framework import generic_test
-
+from list_node import ListNode
 
 def merge_two_sorted_lists(L1, L2):
 
-    L1_pt, L2_pt = L1, L2
+    if not L1:
+        return L2
+    if not L2:
+        return L1
 
+    dummy_head = current_node = ListNode()
 
-    if L1_pt.data <= L2_pt.data:
-        curr_least = L1
-        head = L1
-        L1_pt = L1_pt.next
-    else:
-        curr_least = L2
-        head = L2
+    L1_pointer, L2_pointer = L1, L2
 
+    while min(L1_pointer.data , L2_pointer.data) < float("inf"):
 
-    while L1_pt != L2_pt:
+        if L1_pointer.data < L2_pointer.data:
+            current_node.next = L1_pointer
 
-        if L
+            if L1_pointer.next:
+                L1_pointer = L1_pointer.next
+            else:
+                L1_pointer = ListNode(float("inf"))
 
+        else:
+            current_node.next = L2_pointer
 
-    return head
+            if L2_pointer.next:
+                L2_pointer = L2_pointer.next
+            else:
+                L2_pointer = ListNode(float("inf"))
+
+        current_node = current_node.next
+
+    return dummy_head.next
 
 
 if __name__ == '__main__':
